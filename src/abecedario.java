328ff4f15225d564b2f074830c03bd6b31a8db66
Simranjkaursingh <simranjeet.s381025@cesurformacion.com>
2023-11-08 10:04:28 +0100

    abecedario uploaded

---
+++ src/abecedario.java
@@ -0,0 +1,19 @@
+
+public class abecedario {
+	public static void main(String[] args) {
+		
+		for (int count=97; count<97+27-1;count++) {
+			System.out.print((char)count+" ");
+			
+		}
+		System.out.println();
+		
+		int contador=97;
+		while (contador<97+27-1) {
+			System.out.print((char)contador + " ");
+				contador = contador + 1;
+		}
+		
+	}
+	
+}	
